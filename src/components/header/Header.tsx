9991ec40d1a6771d1a7a2bc1517dd3216103a542
ranga1729 <rangabandara1729@example.com>
2024-12-11 16:03:09 +0530

    Header created

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,20 @@
-import './Header.module.css'
+import styles from './Header.module.css'
+import logo from '../../assets/LedgerLens-logo.png'
 
 function Header () {
-  return (
-    <>
-      <h1>Header</h1>
-    </>
-  )
+  return <div className={styles.headerContainer}>
+    <div className={styles.logoContainer}>
+      <img src={logo} className={styles.logo} />
+    </div>
+    <div className={styles.searchContainer}>
+      <div className={styles.searchBar}>
+        <input type='text' className={styles.input} placeholder='Search...'/>
+        <button className={styles.searchButton}>
+          <p className={styles.searchText}>Search</p>
+        </button>
+      </div>
+    </div>
+  </div>
 
 }
 
